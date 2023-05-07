f92f2b92c9ec7ee70bb1a98e00d49f01c90405f5
azmick <azmi.karakaya@ogr.sakarya.edu.tr>
2023-05-07 16:02:45 +0300

    border colors edited, aboutme page complated.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function() {
   const roundObject = document.querySelector('.profile-pic');
   const img = roundObject.querySelector('img');
   const infoContainer = document.querySelector('.info-container');
+  const container = document.querySelector('.container');
   let originalTop, originalLeft, originalWidth, originalHeight;
 
   if (roundObject) {
@@ -21,20 +22,30 @@ window.onload = function() {
       roundObject.classList.toggle('clicked');
       if (roundObject.classList.contains('clicked')) {
         infoContainer.style.display= 'block';
+        container.style.display = 'block';
         roundObject.style.top = '125px';
         roundObject.style.left = '695px';
         roundObject.style.width = '150px';
         roundObject.style.height = '150px';
         img.style.width = '150px';
         img.style.height = '150px';
+        img.style.borderColor = 'grey'
+        img.style.boxShadow = '0px 0px 10px grey';
+        roundObject.style.borderColor = 'grey';
+        roundObject.style.boxShadow = '0px 0px 10px grey';
       } else {
         infoContainer.style.display= 'none';
+        container.style.display= 'none';
         roundObject.style.top = originalTop;
         roundObject.style.left = originalLeft;
         roundObject.style.width = originalWidth;
         roundObject.style.height = originalHeight;
         img.style.width = '';
         img.style.height = '';
+        img.style.borderColor = '#00ff00'
+        img.style.boxShadow = '0 0 5px rgb(0, 255, 0)';
+        roundObject.style.borderColor = '#00ff00';
+        roundObject.style.boxShadow = '0 0 5px rgb(0, 255, 0)';
       }
     });
 
